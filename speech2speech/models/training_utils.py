48f0039f799ee2e53b8ee20a7db28429f63883be
Bithika Jain <jain.bithika01@gmail.com>
2020-02-03 23:23:11 +0000

    Included model evaluation

--- speech2speech/models/training_utils.py
+++ speech2speech/models/training_utils.py
@@ -1,8 +1,8 @@
 #
 # Copyright (C) 2020 Bithika Jain
 #
-
-
+import os, sys
+import time
 import argparse
 import numpy as np
 import torch
@@ -53,3 +53,44 @@ def train_model(model, optimizer, num_epochs, training_loader, device, checkpoin
         train_res_perplexity.append(perplexity.item())
 
     return train_res_recon_error, train_res_perplexity
+
+def load_checkpoint(model, optimizer, filename):
+    # Note: Input model & optimizer should be pre-defined.  This routine only updates their states.
+    start_epoch = 0
+    if os.path.isfile(filename):
+        print("=> loading checkpoint '{}'".format(filename))
+        checkpoint = torch.load(filename)
+        start_epoch = checkpoint['epoch']
+        model.load_state_dict(checkpoint['state_dict'])
+        optimizer.load_state_dict(checkpoint['optimizer'])
+        print("=> loaded checkpoint '{}' (epoch {})"
+                  .format(filename, checkpoint['epoch']))
+    else:
+        print("=> no checkpoint found at '{}'".format(filename))
+
+    return model, optimizer, start_epoch
+
+
+def eval_model(model, training_loader, device, checkpoint_dir):
+    model.eval() # enter evaluation mode
+    val_res_recon_error = []
+    val_res_perplexity = []
+    epoch_loss = 0
+    with torch.no_grad():
+        for epoch in range(num_epochs):
+            std, _ = std_mean_dataloader(validation_loader)
+            for i, (x, _) in enumerate(validation_loader):
+                batch_X = x.to(device)
+                # forward pass
+                vq_loss, data_recon, perplexity = model(batch_X)
+                recon_error = F.mse_loss(data_recon, x.cuda()) / std
+                loss = recon_error + vq_loss
+                epoch_loss += loss.item()
+
+                if (i+1) % 10 == 0:
+                    print ("Epoch[{}/{}], Step [{}/{}], recon_error: {:.4f}, perplexity: {:.4f}"
+                   .format(epoch+1, num_epochs, i+1, len(training_loader), recon_error.item(), perplexity.item()))
+
+
+            val_res_recon_error.append(recon_error.item())
+            val_res_perplexity.append(perplexity.item())
