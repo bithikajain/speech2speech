d28f4eb939cf1c69d89815c906fa9e2433da0b6b
Bithika Jain <jain.bithika01@gmail.com>
2020-02-04 04:39:40 +0000

    Raw audio to linear spectrogram script

--- speech2speech/data_preprocessing/make_spectrograms.py
+++ speech2speech/data_preprocessing/make_spectrograms.py
@@ -1,3 +1,17 @@
 #
 # Copyright (C) 2020 Bithika Jain
 #
+import os
+import math
+import numpy as np
+import librosa
+import librosa.display
+
+# make spectogram array from trimmmed audio files
+def create_spectrogram_array():
+    for audio_file in file_list:
+        samples, sample_rate = librosa.load(audio_file, sr = 16000)
+        audio_name = audio_file.split('trim_silence_30db')[1].split('/')[-1].split('.')[0]
+        filename  = os.path.join(spectogram_array_path_trim_30db , 'trim_spec'+ '_'+audio_name)
+        X = np.abs(librosa.stft(samples))
+        np.save(filename, X)
