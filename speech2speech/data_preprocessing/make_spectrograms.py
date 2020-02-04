eddea22557763d7e1024f42a6b6d902d46dbbc5d
Bithika Jain <jain.bithika01@gmail.com>
2020-02-04 04:50:49 +0000

    Included ntft= 512 (reccomended for speech processing)

--- speech2speech/data_preprocessing/make_spectrograms.py
+++ speech2speech/data_preprocessing/make_spectrograms.py
@@ -15,3 +15,13 @@ def create_spectrogram_array():
         filename  = os.path.join(spectogram_array_path_trim_30db , 'trim_spec'+ '_'+audio_name)
         X = np.abs(librosa.stft(samples))
         np.save(filename, X)
+
+
+# make spectogram array from trimmmed audio files
+def create_spectrogram_array_ntft():
+    for audio_file in file_list:
+        samples, sample_rate = librosa.load(audio_file, sr = 16384)
+        audio_name = audio_file.split('trim_silence_30db')[1].split('/')[-1].split('.')[0]
+        filename  = os.path.join(spectogram_array_path_trim_30db_ntft512 , 'trim_spec'+ '_'+audio_name)
+        X = np.abs(librosa.stft(samples, ntft = 512))
+        np.save(filename, X)
