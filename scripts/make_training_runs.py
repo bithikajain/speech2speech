9a60cf84400776a4fd2bcedb511eb93a97d95ed1
Bithika Jain <jain.bithika01@gmail.com>
2020-02-06 00:43:38 -0500

    Modified data directory path

--- scripts/make_training_runs.py
+++ scripts/make_training_runs.py
@@ -6,7 +6,7 @@ import itertools
 python /home/ubuntu/speech2speech/scripts/train_model.py --verbose --debug\
     --base-dir "/home/ubuntu/speech2speech/test_num_embeddings256_spectrogram_db_lr1em4"\
     --data-dir "/home/ubuntu/speech2speech/data/raw/VCTK-Corpus"\
-    --spectrogram-dir "/home/ubuntu/speech2speech/data/interim/spectogram_array_path_trim_30db_ntft_512"\
+    --spectrogram-dir "/home/ubuntu/speech2speech/data/interim/spectogram_array_trim_30db"\
     --time_length 350\
     --train-data-fraction 0.8\
     --validation-data-fraction 0.1\
